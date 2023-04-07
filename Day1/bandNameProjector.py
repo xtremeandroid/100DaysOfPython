69b0c0be208bb25525598b1904358aaf93d3b5fe
Ayush Singh <mr.technical19@gmail.com>
2023-04-07 14:00:46 +0530

    startingout

---
+++ Day1/bandNameProjector.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+petName = input("Enter name of your pet\n")
+print("Your Band Name could be" + " " + city + " " + petName)
