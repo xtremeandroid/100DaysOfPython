79b26ea33870a48491cb852c7974afc0cdaa57f3
Ayush Singh <mr.technical19@gmail.com>
2023-08-05 12:14:54 +0530

    updatedFolderStructure

--- Day26 - Phonetic Alphabet/main.py
+++ Day26 - Phonetic Alphabet/main.py
@@ -50,13 +50,13 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+{new_key:new_value for (index, row) in df.iterrows()}
 
 # TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-# word_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+{"A": "Alfa", "B": "Bravo"}
+word_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# user_input = input("Enter to word to generate a list : ").upper()
-# for letter in user_input:
-#     print(f"{letter} : {word_dict[letter]} ")
+user_input = input("Enter to word to generate a list : ").upper()
+for letter in user_input:
+    print(f"{letter} : {word_dict[letter]} ")
